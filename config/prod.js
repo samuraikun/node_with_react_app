0730e11684a558b5418b682f72d2dd482d9dc14b
samuraikun <tazitaziawawa@gmail.com>
2017-11-26 16:54:47 +0900

    tweak google strategy

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,6 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURL: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  googleRedirestURI: 'https://obscure-fortress-50217.herokuapp.com'
 };
