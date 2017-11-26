cb57ace355301a89d7932bfd5b89a62c084fb5df
samuraikun <tazitaziawawa@gmail.com>
2017-11-26 16:41:35 +0900

    finish auth flow

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
-require('./services/passport');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURL);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 
