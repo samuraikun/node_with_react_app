663c28a67247a8761e2b93fcfb087598d179fa6c
samuraikun <tazitaziawawa@gmail.com>
2017-12-10 19:01:39 +0900

    add payment form by using Stripe API

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURL: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  googleRedirestURI: 'https://obscure-fortress-50217.herokuapp.com'
+  googleRedirestURI: 'https://obscure-fortress-50217.herokuapp.com',
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
