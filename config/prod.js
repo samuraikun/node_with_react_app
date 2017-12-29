f3742fa82ba75a538111a58188dbaeb40bf1ce7e
samuraikun <tazitaziawawa@gmail.com>
2017-12-29 16:29:04 +0900

    add sendgrid

--- config/prod.js
+++ config/prod.js
@@ -6,5 +6,6 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY,
   googleRedirestURI: 'https://obscure-fortress-50217.herokuapp.com',
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY
 };
