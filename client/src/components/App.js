9b4caa2e3cdbe5472429a4feb0cdc934e978f1bb
samuraikun <tazitaziawawa@gmail.com>
2018-02-18 17:24:50 +0900

    add Dashboard Component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,8 @@ import * as actions from '../actions';
 
 import Header from './Header';
 import Landing from './Landing';
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SuerveyNew</h2>;
+import Dashboard from './Dashboard';
+const SurveyNew = () => <h2>SurveyNew</h2>;
 
 class App extends Component {
   componentDidMount() {
