8c8862efa11ee053df15e7081c3351bd401e6774
samuraikun <tazitaziawawa@gmail.com>
2018-03-04 15:32:20 +0900

    fix README & cosmetic

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 - Sever: Node.js
 - Client: ES6, React.js
 - DB: MongoDB
+
+## Setup
+
+```javascript
+// root dir
+npm install
+cd client/
+npm install
+
+// run node.js & react
+npm run dev
+```
--- config/prod.js
+++ config/prod.js
@@ -4,7 +4,7 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURL: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  googleRedirestURI: 'https://obscure-fortress-50217.herokuapp.com',
+  googleRedirectURI: process.env.GOOGLE_REDIRECT_URI,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   sendGridKey: process.env.SEND_GRID_KEY,
