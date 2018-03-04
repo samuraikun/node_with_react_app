e75a4609c598eaadd59a07b933aef12d54de85d7
samuraikun <tazitaziawawa@gmail.com>
2018-03-04 16:10:12 +0900

    submit survey by action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,11 +8,13 @@ export const fetchUser = () => async dispatch => {
 };
 
 export const handleToken = (token) => async dispatch => {
-  const res = await axios.post('./api/stripe', token);
+  const res = await axios.post('/api/stripe', token);
 
   dispatch({type: FETCH_USER, payload: res.data });
 };
 
-export const submitSurvey = values => {
-  return { type: 'submit_survey' };
+export const submitSurvey = values => async dispatch => {
+  const res = await axios.post('/api/surveys', values);
+
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
